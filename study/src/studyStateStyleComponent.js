aa3c2fb69691edfdf0d26080c67a4e3111dc871e
jiwon <jidole041214@naver.com>
2020-11-09 22:49:41 +0900

    state 공부

--- study/src/studyStateStyleComponent.js
+++ study/src/studyStateStyleComponent.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import styled from 'styled-components'; // 스타일 컴포넌트 정의
 
-class Multiply extends Component{
+class State extends Component{
     state={
-        name:''
+        name:'',
+        phone:''
     }
     nameChange=(e)=>{
         this.setState({
-            name: e.target.value // 이벤트를 발생시킨 input value 가져옴
+            [e.target.name]:e.target.value // name에 따라 값 변경 타겟이 달라짐
+/*             name: e.target.value // 이벤트를 발생시킨 input value 가져옴 */
+        })
+    }
+    submit=(e)=>{
+        e.preventDefault();
+        this.props.onCreate(this.state);
+        console.log(this.state.name,this.state.phone)
+        this.setState({
+            name:'',
+            phone:''
         })
     }
     render(){
@@ -18,17 +29,28 @@ class Multiply extends Component{
         `; // 스타일 컴포넌트 (앞으로 자주 이용 예정)
 
         return(
-            <Div>
-                <p>이름 : {this.state.name}</p>
-                <input
-                placeholder="이름을 입력하세요"
-                type="name"
-                onChange={this.nameChange}
-                value={this.state.name}
-                />
-            </Div>
+            <form action="submit">
+                <Div>
+                    <p>이름 : {this.state.name}{this.state.phone}</p>
+                    <input
+                    placeholder="이름을 입력하세요"
+                    type="name"
+                    onChange={this.nameChange}
+                    value={this.state.name}
+                    name="name"
+                    />
+                    <input
+                    placeholder="전화번호를 입력하세요"
+                    onChange={this.nameChange}
+                    value={this.state.phone}
+                    name="phone"
+                    />
+                    <button type="submit">등록</button>
+                </Div>
+                이름 : {this.state.name + '/' + this.state.phone}
+            </form>
         )
     }
 }
 
-export default Multiply;
+export default State;
